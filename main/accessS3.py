3f074a79d79a5e6f20cf94a778063c797a23117d
JamieSullivan12 <jamie.sullivan12@icloud.com>
2021-12-27 17:41:30 +1100

    Added functionality to read and write to and from the S3 AWS database

---
+++ main/accessS3.py
@@ -0,0 +1,25 @@
+#accessing AWS bucket
+import boto3
+import os
+
+def s3accesscode():
+
+    #basic credentials 
+    aws_access_key='AKIASKERJ7ZN2VU2TAVB'
+    aws_secret_access = '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'
+
+    #Connect to the AWS s3 service
+    client = boto3.client(
+        's3',
+        aws_access_key_id = aws_access_key,
+        aws_secret_access_key = aws_secret_access,
+        region_name = 'ap-southeast-2'
+    )
+
+    bucket_name = 'test-bucket-71'
+
+    #uploading a file
+    client.upload_file('data/test.csv', bucket_name, 'test_file.csv')
+
+    #downloading a file
+    client.download_file(bucket_name, 'test_file.csv', 'data/downloads/test_file.csv')
