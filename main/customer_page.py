f3971f3545374ab86cdffa8544d80a1d1ec077db
Jamie Sullivan <jamie.p.sullivan12@gmail.com>
2022-01-15 12:58:27 +1100

    Added feature to import all exercise categories and exercises

--- main/customer_page.py
+++ main/customer_page.py
@@ -61,30 +61,27 @@ class CustomerPage(tk.Frame):
 
             table_array.append([self.name_textbox,self.DoB_textbox,self.goals_textbox,self.email_textbox])
 
-        return table_array,customers
+        return customers
 
 
     '''
     FUNCTION: Save all changes the user has made in the customer table
     IN:
-    - customer_df: pandas datastructure containing the raw data (written to if there is a change)
-    - table_array: A 2D array of entry object (textboxes from the table). Accessed to see if the user has made changes
+    - customers: A list of all the customer objects
     OUT:
     - None
     '''
-    def save_changes(self, table_array, customers):
-        # library used when accessing the pandas library (column name required instead of index)
-        
-        save_log = ""
-        changes_made = False
-        cancel = False
-        for c in customers:
-            if cancel==False:
+    def save_changes(self, customers):
 
+        save_log = ""
+        cancel = False
+
+        for c in customers:
+            if cancel==False: #break value
+                # customer object method to check if changes have been made
                 error_msg,items_changed=customers[c].check_difference()
-                
-                if error_msg!="":cancel=True
-                
+                if error_msg!="":cancel=True # if an error occured when checking the differences, break out of the loop
+                #if no errors occured, update all the values for that customer
                 elif len(items_changed)>0 and error_msg=="":
                 
                     self.controller.customer_df.at[customers[c].pandas_index,"Name"]=customers[c].name
@@ -106,60 +103,6 @@ class CustomerPage(tk.Frame):
                 tkinter.messagebox.showerror(title="Unable to save changes", message="Please ensure that the core data file is not open and try again.")
            
 
-
-        '''
-        #loop through each row in the customer datastructure
-        for i,values in self.controller.customer_df.iterrows():
-            # loop through each column for each row in the customer datastructure
-            for g,value in enumerate(values):
-                value = str(value)
-                # if the field being updated is a date, slightly more logic is required
-                if self.indexes[g]=="DoB":
-                    override_cont = False #used to overridde if old date is incorrect format (so it can be changed)
-                    cancel = False # if the new date is incorrect, the change will be cancelled
-                    old_datetime_object = "NaN"
-                    new_datetime_object = "NaN"
-                    try: # making the old date as a datetime object (this is the date that currently sits in the pandas dataset)
-                        old_datetime_object = pd.to_datetime(str(value), format="%Y-%m-%d")
-                    except ValueError as e:
-                        override_cont = True
-                    try: #making the new date as a datetime object (this is the date that the user entered). If it is incorrect, the change will be cancelled.
-                        new_datetime_object = pd.to_datetime(str(table_array[i][g].get()), format="%d/%m/%Y")
-                    except ValueError as e:
-                        cancel = True
-                        tkinter.messagebox.showerror(title="Unable to save changes", message=f"The date entered for {table_array[i][0].get()} is invalid. This customer will not be updated.")
-
-                    if  override_cont or old_datetime_object != new_datetime_object and not cancel:
-                        new_date = new_datetime_object
-                        self.controller.customer_df.at[i,self.indexes[g]]=new_date
-                        save_log += "Customer: " + table_array[i][0].get() + "; Column: " + self.indexes[g] + " -> '" + table_array[i][g].get() + "'\n"
-                        changes_made = True
-                
-                else:
-                    # only save the changes which exist
-                    if value != table_array[i][g].get():
-                        try:
-                            #at the point (i - row,g - column), change the value to what currently sits in the corresponding textbox on the tkinter widget
-                            self.controller.customer_df.at[i,self.indexes[g]]=str(table_array[i][g].get())
-                            save_log += "Customer: " + table_array[i][0].get() + "; Column: " + self.indexes[g] + " -> '" + table_array[i][g].get() + "'\n"
-                            changes_made = True #flag used at the end
-                        except ValueError as e:
-                            # a field cannot be empty because it will throw an error. revert the change if this occurs
-                            tkinter.messagebox.showerror(title="Empty field invalid", message="Customer: " + table_array[i][0].get() + "; Column: " + self.indexes[g] + " - cannot be empty. Your change will now be reverted")
-                            table_array[i][g].insert(tk.END,str(value))
-
-        #logging and error message purposes
-        if changes_made:
-            try:
-                self.controller.customer_df.to_csv("tempdata1.csv", index=False)
-                tkinter.messagebox.showinfo(title="Successfully saved changes", message="Your changes were saved:\n\n" + save_log)
-            except PermissionError as e:
-                tkinter.messagebox.showerror(title="Unable to save changes", message="Please ensure that the core data file is not open and try again.")
-            except Exception as e:
-                tkinter.messagebox.showerror(title="Unable to save changes", message="Unknown error. Please try again and contact the administrator for support.")
-        else:
-            tkinter.messagebox.showinfo(title="No changes were made", message="No changes were made")
-        '''
     def set_heading(self):
         self.controller.tkRoot.title("Training App > Customers")
 
@@ -176,9 +119,9 @@ class CustomerPage(tk.Frame):
         title.grid(row=0,column=0, padx=10, pady=(10,0), sticky="w")
 
         # read data and create table
-        table_array,customers = self.read_customer_data() 
+        customers = self.read_customer_data() 
         # to be used when the user changes any fields in the table on screen
-        save_changes_button = tk.Button(self.frame, text='Save Changes',borderwidth=0,width=15, height=1,font=self.controller.p3, fg="#ffffff", bg=self.controller.buttonBackground,activebackground=self.controller.buttonClick,activeforeground="#ffffff", relief=tk.FLAT, command=lambda:self.save_changes(table_array, customers))
+        save_changes_button = tk.Button(self.frame, text='Save Changes',borderwidth=0,width=15, height=1,font=self.controller.p3, fg="#ffffff", bg=self.controller.buttonBackground,activebackground=self.controller.buttonClick,activeforeground="#ffffff", relief=tk.FLAT, command=lambda:self.save_changes(customers))
         save_changes_button.grid(row=0,column=1, padx=50, pady=15)
         controller.button_hover(save_changes_button,self.controller.buttonClick,self.controller.buttonBackground)
 
